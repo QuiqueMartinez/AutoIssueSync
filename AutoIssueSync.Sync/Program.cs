e72372ff349c606308ff40839dfb7b96e9d4e35b
Quique <e.martinez.bueno@gmail.com>
2024-10-01 14:36:28 +0200

    Force workflow execution

--- AutoIssueSync.Sync/Program.cs
+++ AutoIssueSync.Sync/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using Octokit;  // Librería para interactuar con la API de GitHub
 using AutoIssueSync.Core;
 
